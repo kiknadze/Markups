74da9815dc5a27bc7419c98279fdc2faaf565b1e
Temo Kiknadze <temo.kiknadze@yahoo.com>
2018-11-19 21:00:52 +0400

    Add files via upload

---
+++ precache-manifest.2f0b551d520d4a39d22b0118e14d1d21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2b22b037836fa8944403",
+    "url": "/static/js/main.2b22b037.chunk.js"
+  },
+  {
+    "revision": "e6dad8d08cf8c443a01b",
+    "url": "/static/js/1.e6dad8d0.chunk.js"
+  },
+  {
+    "revision": "2b22b037836fa8944403",
+    "url": "/static/css/main.614f3d2c.chunk.css"
+  },
+  {
+    "revision": "655b13632b3cdbf12faba88319ad1e05",
+    "url": "/index.html"
+  }
+];
